6b668479d159116a2aee0cd2e0f6bcbdbb1627fb
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-16 13:05:37 -0500

    sign in loading

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import SignIn from 'src/components/SignIn'
 
 function App() {
-  return (
-    <div className="App">
-      <p> testhi</p>
-    </div>
-  )
+  return <Route exact path="/" component={SignIn} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignIn from './components/SignIn'
 import App from './containers/App'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SignIn />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
